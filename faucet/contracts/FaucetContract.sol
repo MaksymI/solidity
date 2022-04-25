2721b7e6beae0989b0ef43f8e66ff46f7e587c3f
MaksymI <maksym.izmailov.lv@gmail.com>
2022-04-25 20:14:20 +0300

    jsonrpc

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -11,4 +11,15 @@ contract Faucet {
     // which means they can be called via contracts and other txs
     receive() external payable {}
     function addFunds() external payable {}
+
+    function justTesting() external pure returns(uint) {
+        return 2 + 2;
+    }
+    // pure, view - read-only, no gas free
+    // view - indicates that the function will not alter the storage state in any way
+    // pure - even more strict, indicating that it won't even read the storage state
+
+    // transactions (can generate state changes) and require gas fee
+
+    // 
 }
